ffe36302575751c2495fae08ea0fd4e86d190e78
Feela <135992570+FeeIa@users.noreply.github.com>
2025-08-10 07:03:09 +0800

    Update ExampleUsage.server.lua

--- src/ReplicatedStorage/NPCService/ExampleUsage.server.lua
+++ src/ReplicatedStorage/NPCService/ExampleUsage.server.lua
@@ -4,12 +4,16 @@ local model: Model = Path.To.Your.Model
 local controller = NPCService:AddNPC(
 	model,
 	{
-		-- You only need to pass what value you want custom, everything defaults to false
+		-- You only need to pass what value you want customize, everything defaults to false
 		isAggressive = true
 	},
 	{
-		-- The parameter pathConfig has a default value of Roblox's default AgentParams, specify if want custom.
-		WaypointSpacing = 2 -- Recommended value: Above 2, Roblox's default is 4
+		-- The parameter pathConfig defaults to Roblox's default AgentParams for PathfindingService, specify if want custom.
+		WaypointSpacing = 2 -- Recommended value: >= 2, Roblox's default is 4
+	},
+	{
+		AttackCooldown = 3,
+		AttackRange = 5
 	}
 )
 
